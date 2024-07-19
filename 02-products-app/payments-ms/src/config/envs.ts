dfd75151916e5cc89bef9e4df3ab8d9d99d8e569
CJavat <cdpm98@hotmail.com>
2024-07-19 06:55:06 -0600

    SECCIÓN 11 / CAP 5 - EN CURSO

--- 02-products-app/payments-ms/src/config/envs.ts
+++ 02-products-app/payments-ms/src/config/envs.ts
@@ -7,6 +7,7 @@ interface EnvVars {
   STRIEP_SUCCESS_URL: string;
   STRIEP_CANCEL_URL: string;
   STRIPE_ENDPOINT_SECRET: string;
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
@@ -16,10 +17,14 @@ const envsSchema = joi
     STRIEP_SUCCESS_URL: joi.string().required(),
     STRIEP_CANCEL_URL: joi.string().required(),
     STRIPE_ENDPOINT_SECRET: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 if (error) throw new Error(`Config validation error: ${error.message}`);
 
 const envVars: EnvVars = value;
@@ -30,4 +35,5 @@ export const envs = {
   striepSuccessUrl: envVars.STRIEP_SUCCESS_URL,
   striepCancelUrl: envVars.STRIEP_CANCEL_URL,
   stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
+  natsServers: envVars.NATS_SERVERS,
 };
